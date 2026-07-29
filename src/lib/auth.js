605cc817fcd9bbc4b7923e764a8c07a3a525d503
saadmannur <saadmannur99@gmail.com>
2026-07-29 17:27:41 +0600

    google signin implement

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -16,6 +16,13 @@ export const auth = betterAuth({
         enabled: true,
     },
 
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
+
     user: {
         additionalFields: {
             role: {
